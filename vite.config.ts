2426743ef5c386d71e3e1ce37178a68f2133a63d
BeataMaro <beatamaro@interia.pl>
2024-02-11 21:27:31 +0100

    config: install @types/dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,32 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
+import { defineConfig  } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-    css: true,
-    coverage: {
-      provider: 'c8',
-      all: true,
-      reporter: 'text',
-      exclude: [
-        '**/node_modules/**',
-        '**/dist/**',
-        '**/models/**',
-        '**/.{idea,git,cache,output,temp}/**',
-        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-        '**/*.d.ts',
-        '**/.{eslint,mocha,prettier}rc.{js,cjs,yml}',
-        'src/main.tsx',
-      ],
+export default defineConfig(() => {
+
+  return {
+    plugins: [svgr(), react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/setupTests.ts'],
+      css: true,
+      coverage: {
+        provider: 'c8',
+        all: true,
+        reporter: 'text',
+        exclude: [
+          '**/node_modules/**',
+          '**/dist/**',
+          '**/models/**',
+          'src/main.tsx',
+        ],
+      },
     },
-  },
+  }
 })
